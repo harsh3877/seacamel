b27e15686e5c8378b8aae73ee22846a74a1d53c2
harsh3877 <67279685+harsh3877@users.noreply.github.com>
2020-11-30 12:21:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="img/png" href="images/fevicon.png">
+	<title>Sea Camel- All sea vehicle</title>
+	<meta charset="utf-8"> 
+<body >
+<header>
+<?php
+include("header.php");
+?>
+
+<div id="demo" class="carousel slide mt-5" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+  </ul>
+  <div class="carousel-inner">
+	<div class="carousel-item active">
+      <img src="slider\slider4.png" alt="Boat" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="font-family:Harrington;" data-aos="fade-right" data-aos-duration="1500"><b>Water in the boat is the ruin of the boat, but water under the boat is its support.</b></h3>
+      </div>   
+    </div>
+    <div class="carousel-item ">
+      <img src="slider\slider1.jpg" alt="Boat" width="1100" height="500">
+		<div id="boatline"  class="carousel-caption">
+			<h3 style="font-family:Harrington;" data-aos="fade-right" data-aos-duration="1500"><b>Boat Safety You Need, Load Limits You Shouldn't Exceed</b></h3>
+        
+		</div>   
+    </div>
+    <div class="carousel-item">
+      <img src="slider\slider2.jpg" alt="Ship" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="font-family:Harrington;" data-aos="fade-right" data-aos-duration="1500"><b>Feel Like a Person, Not a Passenger.</b></h3>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="slider\slider3.jpg" alt="Submarine" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="font-family:Harrington;" data-aos="fade-right" data-aos-duration="1500"><b>Under Water Diver</b></h3>
+      </div>   
+    </div>
+	<div class="carousel-item">
+      <img src="slider\slider5.jpg" alt="boat" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="font-family:Harrington;" data-aos="fade-right" data-aos-duration="1500"><b>A boat without captain cannot remain long on the surface!</b></h3>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+</header>
+
+<section>
+	<div class="container-fluid " style="background:#f2f2f2;">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-6">
+					<img class="rounded-circle" data-aos="fade-right" src="images\websitelogo2.png" width="100%">
+				</div>
+				<div class="col-12 col-md-6 col-lg-6 pt-5 aboutfont">
+					<h3 class=" mt-4 mb-3  " data-aos="fade-in" style="font-family:time new roman; color:#062B91; "><b>About Sea Camel<b> </h3>
+					<h4 data-aos="fade-right" style="text-align:justify;font-weight:normal;"><sup><span class="fa fa-quote-left mr-2"></span></sup>Sea Camel is the company that perovide you with all the information on all water vehicles and
+												we are the leading company in providing informaton on all the water vehicles in brief. </h4>
+					<h4 data-aos="fade-right" data-aos-delay="200" style="text-align:justify;font-weight:normal; ">This will help people to get precise and relevent information on this sector. Any person willing 
+												to start a startup can take information from our website. specially people of costal areas will 
+												get a good knowledge in water vehicles sector and can expand their business.<sup><span class="fa fa-quote-right ml-2"></span></sup>	</h4>
+				</div>								
+			</div>
+		</div>
+	</div>
+	<!--section 2-->
+	<!--<div class="container-fluid pt-5 pb-5 listbg" style="/*background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5),rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('boatimages/listbg.jpg')no-repeat; background-size: cover; background-attachment: fixed; */">
+		<div class="container listcontainer">
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-6">
+					<span class="text-light"><h4 style="font-family:Harrington; display:inline-block;" data-aos="fade-in"><b>Points to cover</b></h4> 
+				</div>
+				<div class="col-12 col-md-6 col-lg-6 listimg" style="background-image:url('images/list2.png'); background-repeat: no-repeat; background-size: contain;">
+				
+				</div>
+			</div>
+		</div>
+	</div>-->
+	
+	<!--section 3-->
+<div class="container-fluid info" style="background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5),rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('slider/listbg.jpg')no-repeat; background-size: cover; background-attachment: fixed; ">
+	<div class="container-fluid py-5">
+		<div class="row mr-0 py-5 ">
+			<div class="col-12 col-md-6 col-lg-6 vehiclefont text-light" style="display:inline-block;">
+				<h4 data-aos="fade-up" style="text-align:justify;font-weight:normal;">Watercraft, also known as water
+				vessels or waterborne vessels, are vehicles used in water, including ships, boats, hovercraft and
+				submarines. Watercraft usually have a propulsive capability (whether by sail, oar, paddle or engine) 
+				and hence are distinct from a simple device that merely floats, such as a log raft.</h4>
+					
+			</div>
+			
+			<div class="col-12 col-md-6 col-lg-2 ">
+						<h5><ul type="none" style="font-family:time new roman; color:#000; display:inline-block;" class="text-light">
+								<li data-aos="fade-right" data-aos-delay="500"><span class="fa fa-ship text-light pr-2"></span> Unpowered</li>
+									<br><ul type="none" data-aos="fade-in"  data-aos-delay="700">
+										<li >- rafts</li>
+										<li>- gondolas</li>
+										<li>- kayaks </li>
+										<li>- etc</li>
+									</ul>
+							</ul>
+			</div>
+			<div class="col-12 col-md-6 col-lg-2">
+						
+						<h5><ul type="none" style="font-family:time new roman; color:#000; display:inline-block;" class="text-light">
+								<li data-aos="fade-right" data-aos-delay="700" ><span class="fa fa-ship text-light pr-2"></span> Sailboats </li>
+								<br><ul type="none"  data-aos="fade-in"  data-aos-delay="900">
+										<li>- Ketch</li>
+										<li>- yawl</li>
+										<li>- cutter</li>
+										<li>- etc</li>
+									</ul>
+							</ul>
+			</div>
+			<div class="col-12 col-md-6 col-lg-2">
+						<h5><ul type="none" style="font-family:time new roman; color:#000; display:inline-block;" class="text-light">
+								<li data-aos="fade-right" data-aos-delay="900" ><span class="fa fa-ship text-light pr-2"></span> Motorboats </li>
+								<br><ul type="none"  data-aos="fade-in"  data-aos-delay="1100">
+										<li>- Bowrider</li>
+										<li>- Center Console</li>
+										<li>- Bass boat</li>
+										<li>- etc</li>
+									</ul>
+						<ul></h5></span>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<!--section 4-->
+<div class="container-fluid" style="background:#f2f2f2;">
+	<div class="container py-5">
+		<div class="row mr-0 py-5">
+			<div class="col-12 col-md-6 col-lg-6 " style="display:inline-block;">
+				<img class="rounded" data-aos="fade-in" src="slider\fef.png" style="width:100%;">
+			</div>
+			
+			<div class="col-12 col-md-6 col-lg-6 vehiclefont">
+				<h3 class=" mt-4 mb-3  " data-aos="fade-in" style="font-family:time new roman; color:#062B91; "><b>Contact us</b> </h3>
+					<h4 data-aos="fade-up" style="text-align:justify;font-weight:normal;">Sea camel provide all visitiors a contact section
+									where any one can ask queries and give suggestion regarding to development of website. All teh queries 
+									and sugession will be taken into consideration as soon as possibel.</h4>
+					<a href="contact.php" data-aos="fade-up" class="btn btn-outline-primary mt-3">Contact Us</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!--Section 5-->
+<div class="container-fluid" style="">
+	<div class="container py-5">
+		<div class="row mr-0 py-5">
+			<div class="col-12 col-md-6 col-lg-6 vehiclefont mb-4">
+				<h3 class=" mt-3 mb-3  " data-aos="fade-in" style="font-family:time new roman; color:#062B91; "><b>Vehicles</b> </h3>
+					<h4 data-aos="fade-up" style="text-align:justify;font-weight:normal;">Information on all water vehicles will be provided here
+															no need to check different websites. All the infromation will bw accurate and precise.
+															You can trust on us in aspect of detalis provided to you on our website.</h4>
+					<a href="vehicles.php" data-aos="fade-up" class="btn btn-outline-primary mt-3">Show More</a>
+			</div>
+			<div class="col-12 col-md-6 col-lg-6 " style="display:inline-block;">
+				<img class="rounded" data-aos="fade-in" src="slider/homevehicle.png" style="width:100%; height:100%;">
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<?php
+include("calltoaction.php");
+include_once("testi.php");
+include("footer.php");
+?>	
+
+
+<!---------------->
+	
+</body>
+</html>
